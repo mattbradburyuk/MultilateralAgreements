ba28f804594df0c3bbf9ff4ba7be23c2dd566dc6
mattbradburyuk <matthew.bradbury@me.com>
2019-02-07 10:08:16 +0000

    agreeAgreement flow added, need to add Status and test for status change

--- workflows/src/main/kotlin/com/multilateralagreements/workflows/AgreementContractFlows.kt
+++ workflows/src/main/kotlin/com/multilateralagreements/workflows/AgreementContractFlows.kt
@@ -4,9 +4,12 @@ import co.paralleluniverse.fibers.Suspendable
 import com.multilateralagreements.contracts.AgreementContract
 import com.multilateralagreements.contracts.AgreementState
 import net.corda.core.contracts.TransactionState
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
@@ -15,18 +18,21 @@ import net.corda.core.utilities.ProgressTracker
 // * Flows *
 // *********
 
+// Create Agreement flows
 
 @InitiatingFlow
 @StartableByRPC
 class CreateAgreementFlow(val agreement: String, val otherParty: Party): FlowLogic<SignedTransaction>(){
 
-
     override val progressTracker = ProgressTracker()
-
-
 
     @Suspendable
     override fun call(): SignedTransaction {
+
+
+        // TODO where does logging go to
+
+        logger.info("MB: test log message")
 
         val txBuilder = TransactionBuilder()
 
@@ -51,9 +57,7 @@ class CreateAgreementFlow(val agreement: String, val otherParty: Party): FlowLog
 
         return subFlow(FinalityFlow(stx, listOf(session)))
 
-
    }
-
 }
 
 
@@ -79,4 +83,80 @@ class CreateAgreementResponderFlow(val otherPartySession: FlowSession): FlowLogi
 
     }
 
+}
+
+
+// Agree agreement Flows
+
+
+@InitiatingFlow
+@StartableByRPC
+class AgreeAgreementFlow(val linearId: UniqueIdentifier, val otherParty: Party): FlowLogic<SignedTransaction>(){
+
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+
+        // find input state
+
+        val criteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
+        val result = serviceHub.vaultService.queryBy<AgreementState>(criteria)
+        val inputStateAndRef = result.states.first()
+        val inputTxState = inputStateAndRef.state
+        val inputState = inputTxState.data
+
+       // create output state
+
+        val outputState = inputState.copy()
+        val outputTxState = inputTxState.copy(data = outputState)
+
+        //create command and signers
+
+        val command = AgreementContract.Commands.Agree()
+
+        val signers = inputState.participants.map{it.owningKey}
+
+        // get notary from input state
+
+        val notary = inputTxState.notary
+
+        // build transaction
+
+        val txBuilder = TransactionBuilder()
+        txBuilder.notary = notary
+        txBuilder.addInputState(inputStateAndRef)
+        txBuilder.addCommand(command, signers)
+        txBuilder.addOutputState(outputTxState)
+
+        txBuilder.verify(serviceHub)
+
+        // Sign and finalise
+
+        val pstx = serviceHub.signInitialTransaction(txBuilder)
+        val session = initiateFlow(otherParty)
+        val stx: SignedTransaction = subFlow(CollectSignaturesFlow(pstx,listOf(session)))
+
+        return subFlow(FinalityFlow(stx, listOf(session)))
+
+    }
+}
+
+@InitiatedBy(AgreeAgreementFlow::class)
+class AgreeAgreementResponderFlow(val otherPartySession: FlowSession): FlowLogic<SignedTransaction>(){
+
+    @Suspendable
+    override fun call(): SignedTransaction{
+
+        val signTransactionFlow = object: SignTransactionFlow(otherPartySession){
+
+            override fun checkTransaction(stx: SignedTransaction){
+                val output = stx.tx.outputs.single().data
+                requireThat { "output is an AgreementState" using  (output is AgreementState) }
+            }
+        }
+        val txId = subFlow(signTransactionFlow).id
+
+        return subFlow(ReceiveFinalityFlow(otherPartySession, expectedTxId = txId))
+    }
 }
--- workflows/src/test/kotlin/com/multilateralagreements/workflows/tests/AgreementContractFlowsTest.kt
+++ workflows/src/test/kotlin/com/multilateralagreements/workflows/tests/AgreementContractFlowsTest.kt
@@ -3,6 +3,8 @@ package com.multilateralagreements.workflows.tests
 import com.multilateralagreements.workflows.CreateAgreementFlow
 import com.multilateralagreements.workflows.CreateAgreementResponderFlow
 import com.multilateralagreements.contracts.AgreementState
+import com.multilateralagreements.workflows.AgreeAgreementFlow
+import com.multilateralagreements.workflows.AgreeAgreementResponderFlow
 import net.corda.core.node.services.queryBy
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.node.MockNetwork
@@ -29,6 +31,7 @@ class AgreementContractFlowsTest {
     init {
         listOf(a, b).forEach {
             it.registerInitiatedFlow(CreateAgreementResponderFlow::class.java)
+            it.registerInitiatedFlow(AgreeAgreementResponderFlow::class.java)
         }
     }
 
@@ -55,6 +58,37 @@ class AgreementContractFlowsTest {
         val result = b.services.vaultService.queryBy<AgreementState>()
 
         assert(result.states[0].ref.txhash == returnedTx.id)
+
+    }
+
+    @Test
+    fun `agree agreement flow test`() {
+
+        // need to setup create transaction
+
+        val flow1 = CreateAgreementFlow("This is a mock agreement", partyb)
+        val future1 = a.startFlow(flow1)
+        network.runNetwork()
+        val returnedTx1 = future1.getOrThrow()
+
+        val state = returnedTx1.coreTransaction.outputStates.first() as AgreementState
+        val linearId = state.linearId
+
+        // agree
+
+        val flow2 = AgreeAgreementFlow(linearId, partyb)
+        val future2 = a.startFlow(flow2)
+        network.runNetwork()
+        val returnedTx2 = future2.getOrThrow()
+
+
+        assert(returnedTx2.toLedgerTransaction(a.services).outputs.single().data is AgreementState)
+
+        // todo: add test to check second transaction has been successful
+
+//        val result = b.services.vaultService.queryBy<AgreementState>()
+//
+//        assert(result.states[0].ref.txhash == returnedTx.id)
 
     }
 }
